bb9120a5b1eb5619e676fbef76099cab58bffe8e
Joseph Tongay <joey.tongay@silvercar.com>
2023-12-01 17:14:31 -0600

    First solution down for day 1. The code is a mess but it works. Come back and refactor in a bit maybe after part 2

--- 01/solution.go
+++ 01/solution.go
@@ -3,13 +3,48 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 func main() {
+	var onlyNumbersRegex = regexp.MustCompile(`[a-zA-Z]+`)
 	contents, err := os.ReadFile("input.txt")
 	if err != nil {
 		fmt.Println("File reading error", err)
 		return
 	}
-	fmt.Println("Contents of file:", string(contents))
+
+	stringContents := string(contents[:])
+	test := strings.Split(stringContents, "\n")
+	var total int = 0
+	for _, calibrationValue := range test {
+		onlyNumbersString := onlyNumbersRegex.ReplaceAllString(calibrationValue, "")
+		calibrationNumber, bad := strconv.Atoi(onlyNumbersString)
+		if bad != nil {
+			fmt.Println("Can't convert string to num", bad)
+			return
+		}
+		if len(onlyNumbersString) == 1 {
+			singleStringNumCombined := fmt.Sprintf("%d%d", calibrationNumber, calibrationNumber)
+			numToAdd, numError := strconv.Atoi(singleStringNumCombined)
+			if numError != nil {
+				fmt.Println("Failure to convert string to num", numError)
+				return
+			}
+			total += numToAdd
+		} else {
+			firstAndLast := fmt.Sprintf("%s%s", onlyNumbersString[0:1], onlyNumbersString[len(onlyNumbersString)-1:])
+			numToAdd, numError := strconv.Atoi(firstAndLast)
+			if numError != nil {
+				fmt.Println("Failure to convert string to num", numError)
+				return
+			}
+			total += numToAdd
+		}
+	}
+	fmt.Println("Done!")
+	fmt.Println(total)
+	fmt.Println("Done!")
 }
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Advent of Code 2023
 
 ## AoC GO 2023
+
+I wanted to challenge myself to learn a new language, and I've been interested in Go for a while, so I decided to use Advent of Code 2023 as an opportunity to learn it.
+On top of that, I've been wanting to get more comfortable in vim/neovim, so I'm using this as an opportunity to learn that as well. I will partially use neovim and partially use GoLand if I get frustrated with vim 🥴.
+
+I have never written a single line of Go before, and I have a full time job along with a 4-month-old at this point in time, so I'm not sure how far I'll get, but I'm going to try my best!
